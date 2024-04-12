cf46378d498b3e555d21910b8bf31c4879d01aea
fishman123456 <den9152@yandex.ru>
2024-04-12 12:16:17 +0700

    присоединена форма добавления

--- ChemicalElementsDictionaryMVC/Pages/ChemicalElements/ChemicalElementsForm.cshtml.cs
+++ ChemicalElementsDictionaryMVC/Pages/ChemicalElements/ChemicalElementsForm.cshtml.cs
@@ -1,3 +1,5 @@
+using ChemicalElementsDictionaryMVC.Model;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,21 @@ namespace ChemicalElementsDictionaryMVC.Pages.ChemicalElements
 {
     public class ChemicalElementsFormModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ChemicalElement ChemicalElement { get; set; } = new();
+
+        public ChemicalElementsFormModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _db.Chemicals.AddAsync(ChemicalElement);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("ChemicalElementsList"); // перенаправление запроса на список элементов
         }
     }
 }
